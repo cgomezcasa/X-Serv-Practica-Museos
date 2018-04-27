e2be83d614074b63e44bceed35c3658901baa355
cgomezcasa <cayegomez1995@gmail.com>
2018-04-27 16:28:56 +0200

    Primera version de models

---
+++ myproject/museoApp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from museoApp.models import Museo
+from museoApp.models import Content_User
+from museoApp.models import Comentario
+from museoApp.models import Configuracion
---
+++ myproject/museoApp/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Museo(models.Model):
+    ID = models.IntegerField(max_length=32)
+    nombre = models.CharField(max_length=128)
+    descripcion = models.TextField()
+    horario = models.TextField()
+    transporte = models.TextField()
+    accesibilidad = models.BinaryField()    #django models
+    url = URLField()
+    distrito = models.CharField(max_length=32)
+    email = models.EmailField(max_length=64)
+
+class Content_User(models.Model):
+    museo = models.ForeignKey(Museo)
+    usuario = models.CharField(max_length=32)
+
+class Comentario(models.Model):
+    museo = models.ForeignKey(Museo)
+    comentario = models.TextField()
+    publicacion = models.DateTimeField()    #django models
+
+class Configuracion(models.Model):
+    fuente = models.CharField(max_length=32)    #posible revision
+    color = models.CharField(max_length=32)
+    titulo = models.CharField(max_length=128)
+    usuario = models.CharField(max_length=32)
+

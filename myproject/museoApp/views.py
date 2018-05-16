1362d805263f4cef2827e6f71d41443c382dbc93
cgomezcasa <cayegomez1995@gmail.com>
2018-05-16 19:47:49 +0200

    parser

--- myproject/museoApp/views.py
+++ myproject/museoApp/views.py
@@ -8,20 +8,20 @@ def xmlParser(request):
     print("Estoy en parser")
     xml_Url = "https://datos.madrid.es/portal/site/egob/menuitem.ac61933d6ee3c31cae77ae7784f1a5a0/?vgnextoid=00149033f2201410VgnVCM100000171f5a0aRCRD&format=xml&file=0&filename=201132-0-museos&mgmtid=118f2fdbecc63410VgnVCM1000000b205a0aRCRD&preview=full"
     page = urlopen(xml_Url)
-    print("page: " + str(page))
     tree = ET.parse(page)
-    print("tree: " + str(tree))
     root = tree.getroot()
-    
-    for i in root.findall('contenidos'):
-        for j in i.findall('atributos'):
-            for atributo in j.findall('atributo'):
-                name = atributo.find(nombre="NOMBRE").text
-                print ("name: " + name)
-                descripcion = atributo.find(nombre="DESCRIPCION").text
-                print ("descripcion: " + descripcion)        
-                distrito = atributo.find(nombre="DISTRITO").text
-                print ("distrito: " + distrito)
-                resp = str(name) + str(descripcion) + str(distrito)
-                return HttpResponse(resp, "200 OK")
+   
+    for i in root.iter('contenido'):
+        for j in i.iter('atributos'):
+            for k in j.findall('atributo'):
+                try:
+                    name = k.find('[@nombre="NOMBRE"]').text
+                    print ("name:" + name)
+                    descripcion = k.find('[@nombre="DESCRIPCION-ENTIDAD"]').text
+                    print ("descripcion: " + descripcion)        
+                    distrito = k.find('[@nombre="DISTRITO"]').text
+                    print ("distrito: " + distrito)
+                except AttributeError:
+                    pass
+        
 
